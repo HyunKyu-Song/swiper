99968a4d83b73abc2738910dbf8e53d846e50ff5
HyunKyu-Song <thdgusrb@gamil.com>
2023-12-24 22:57:29 +0900

    이미지, 소리, 스토리 완성

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-var swiper = new Swiper(".mySwiper", {
-   speed: 400,
-   parallax: true,
+const swiper = new Swiper(".mySwiper", {
+   slidesPerView: 1,
+   spaceBetween: 10,
+   // loop: true,
    pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -9,12 +10,59 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
    },
+   slideActiveClass: 'swiper-slide-active'
 });
 
-const audio = new Audio('./assets/audio/추격전_bgm.wav');
+
+const audio = new Audio('./assets/audio/ending_bgm.wav');
+const crash = document.querySelector('.crash');
+const tom = document.querySelector('.tom');
+const jerry = document.querySelector('.jerry');
+const ending = document.querySelector('.ending');
+
+
+crash.addEventListener('click', function () {
+
+   tom.src = './assets/img/crash.png';
+
+   setTimeout(() => {
+      tom.src = './assets/img/tom2.gif'
+   }, 2000);
+
+   setTimeout(() => {
+      tom.src = './assets/img/tom.gif';
+   }, 10000)
+})
 
 
 document.querySelector('.bgm').addEventListener('click', function () {
-   audio.play();
-   audio.loop = true;
+
+   tom.src = ''
+
+   setTimeout(() => {
+      tom.src = './assets/img/총1.png'
+   }, 1000);
+
+   setTimeout(() => {
+      jerry.src = './assets/img/넘어짐2.png'
+   }, 2000);
+
+   setTimeout(() => {
+      tom.src = './assets/img/총2.png'
+      audio.play();
+   }, 3000);
+
+   setTimeout(() => {
+      ending.style.opacity = 1;
+      ending.style.zIndex = 50;
+      document.querySelector('body').style.filter = 'grayscale(100%)'
+   }, 4000);
+
+   setTimeout(() => {
+      ending.src = './assets/img/ending3.png'
+   }, 7000);
+
+   setTimeout(() => {
+      ending.src = './assets/img/ending2.png'
+   }, 9000);
 })
